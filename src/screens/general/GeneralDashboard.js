8a9ab66695c1720efb8843873751dab910a8d154
Andi Halim <andihalim@berkeley.edu>
2020-10-31 17:40:58 -0400

    general dashboard cleanup last step

--- src/screens/general/GeneralDashboard.js
+++ src/screens/general/GeneralDashboard.js
@@ -3,19 +3,13 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../../styles/GeneralOwnerDashboard.css';
 import RightArrow from '../../assets/right_arrow.png';
-// import ProductionEquivalenciesChart from '../../components/ProductionEquivalenciesChart';
-import DashboardCard1 from '../shared/components/DashboardCard1';
 
 class GeneralOwnerDashboard extends React.PureComponent {
-  /* dash-solar-details will eventually be its own graph component
-     so it'll be easy to write a ternary operator that will render
-     it when it's loaded.
-  */
   render() {
     return (
       <div className="dashboard">
-        <div className="dashboard-content">
-          <div className="dash-announcements-cont">
+        <div className="dashboard-box">
+          <div className="dashboard-content">
             <div className="header-button">
               <div className="header-only">
                 <h3>Page 1</h3>
@@ -30,7 +24,6 @@ class GeneralOwnerDashboard extends React.PureComponent {
                 </Link>
               </div>
             </div>
-            <DashboardCard1 />
           </div>
         </div>
       </div>
