3e1797302f212193e3e1ad12e6506ab4fd7c7c4e
Phoebe Li <liphoebe01@gmail.com>
2020-10-31 13:27:44 -0700

    remove onboarding step files

--- src/screens/onboarding/onboardingData.js
+++ src/screens/onboarding/onboardingData.js
@@ -1,10 +1,7 @@
 import ContactInfoStep from './steps/ContactInfoStep';
 import BasicInfoStep from './steps/BasicInfoStep';
-// import ProjectGroupStep from './steps/ProjectGroupStep';
-// import PaymentStep from './steps/PaymentStep';
 import CompleteStep from './steps/CompleteStep';
 import BylawStep from './steps/BylawStep';
-// import PaymentDetailsStep from './steps/PaymentDetailsStep';
 
 // Maps Step to Component and Fields via Indices
 const OnboardingData = [
@@ -44,31 +41,12 @@ const OnboardingData = [
       'Tell us some general contact information so we can get started setting up your account.',
     header: 'Welcome Aboard!'
   },
-  // {
-  //   component: ProjectGroupStep,
-  //   fields: ['projectGroupId'],
-  //   copy:
-  //     'Project groups in People Power represent the different communities involved in our cooperative. ',
-  //   header: 'Select your project group'
-  // },
   {
     component: BylawStep,
     fields: ['bylaw1', 'bylaw2'],
     copy: '',
     header: 'Owner Agreement and Acknowledgment'
   },
-  // {
-  //   component: PaymentDetailsStep,
-  //   fields: ['isReceivingDividends', 'numberOfShares'],
-  //   copy: '',
-  //   header: 'Purchase shares'
-  // },
-  // {
-  //   component: PaymentStep,
-  //   fields: [],
-  //   copy: '',
-  //   header: 'Finish Payment'
-  // },
   {
     component: CompleteStep,
     fields: [],
