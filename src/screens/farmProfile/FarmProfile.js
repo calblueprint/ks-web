103d5a7516cb4bdf7a7c3380fceb42c821ce910c
Phoebe Li <liphoebe01@gmail.com>
2020-11-21 23:37:27 -0800

    finish farm profile page static version

--- src/screens/farmProfile/FarmProfile.js
+++ src/screens/farmProfile/FarmProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FarmContactCard from './components/FarmContactCard';
+import leftArrow from '../../assets/left_arrow.png';
 import '../../styles/FarmProfile.css';
 
 class FarmProfile extends React.Component {
@@ -12,14 +13,43 @@ class FarmProfile extends React.Component {
 
   componentDidMount() {
     const { match } = this.props;
-    const {id} = match.params;
+    const { id } = match.params;
     this.setState({ id });
   }
 
   render() {
     return (
-      <div>
-        <FarmContactCard id="123" farmName="Golden Corn" />
+      <div className="farm-profile">
+        <div className="farm-profile__back-button">
+          <img
+            className="farm-profile__back-button-icon"
+            src={leftArrow}
+            alt="back arrow"
+          />
+          Back to Farm Search
+        </div>
+        <h1 className="farm-profile__header">Farm Name</h1>
+
+        <div className="farm-profile__body">
+          <div className="farm-profile__body__left">
+            <FarmContactCard
+              id="123"
+              farmerName="Farmer Name"
+              phone="(xxx) - xxx - xxxx"
+              email="farmerfarmer@farmer.com"
+              address="xxxxx Farmer Lane Kauai, HI, xxxxx"
+              gapCertified
+              certificationDate="xxxx"
+              inspector="Lisa Rhoden"
+            />
+          </div>
+
+          <div className="farm-profile__body__right">
+            <div className="farm-profile__graph hover-background">Data</div>
+            <div className="farm-profile__graph hover-background">Data</div>
+            <div className="farm-profile__graph hover-background">Data</div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/screens/farmProfile/components/FarmContactCard.js
+++ src/screens/farmProfile/components/FarmContactCard.js
@@ -1,14 +1,78 @@
 import React from 'react';
+import farmProfileCover from '../../../assets/farmProfileCover.png';
+import farmProfilePhoto from '../../../assets/farmProfilePhoto.png';
+import checkIcon from '../../../assets/checkIcon.png';
+import cancelIcon from '../../../assets/cancelIcon.png';
+import userIcon from '../../../assets/userIcon.png';
+
+import '../../../styles/FarmProfile.css';
 
 class FarmContactCard extends React.PureComponent {
   render() {
     // const { id, farmName, farmerName, phone, email, address, location, inspector } = this.props;
-    const { id, farmName } = this.props;
+    const {
+      id,
+      farmerName,
+      phone,
+      email,
+      address,
+      gapCertified,
+      certificationDate,
+      inspector
+    } = this.props;
 
     return (
-      <div className="farm-card" id={id}>
-        <h3 className="farm-card__header">{farmName}</h3>
-        <div className="farm-card__body">{`id=${id}`}</div>
+      <div className="farm-contact-card" id={id}>
+        <div className="farm-contact-card__main hover-background">
+          <img
+            className="farm-contact-card__main__cover-photo"
+            src={farmProfileCover}
+            alt="farm profile cover"
+          />
+          <img
+            className="farm-contact-card__main__profile-photo"
+            src={farmProfilePhoto}
+            alt="farm profile"
+          />
+
+          <div className="farm-contact-card__main__text">
+            <div className="farm-profile__subheader">{farmerName}</div>
+            <div className="farm-contact-card__main__text__details">
+              <div>{`Phone: ${phone}`}</div>
+              <div>{`Email: ${email}`}</div>
+              <div>{`Address: ${address}`}</div>
+              <div className="farm-contact-card__main__text__details__certification">
+                {gapCertified ? (
+                  <div>
+                    <img
+                      className="farm-contact-card__certification-icon"
+                      src={checkIcon}
+                      alt="certification"
+                    />
+                    {`GAP Certified since ${certificationDate}`}
+                  </div>
+                ) : (
+                  <div>
+                    <img src={cancelIcon} alt="certification" />
+                    Not GAP Certified
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="farm-contact-card__inspector hover-background">
+          <div className="farm-profile__subheader">NSEVP Inspector</div>
+          <div className="farm-contact-card__inspector__body">
+            <img
+              className="farm-contact-card__user-icon"
+              src={userIcon}
+              alt="user"
+            />
+            {inspector}
+          </div>
+        </div>
       </div>
     );
   }
