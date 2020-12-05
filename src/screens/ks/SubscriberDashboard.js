41bcc34a0956db92b05e852231ac5f6dc4c83efe
Phoebe Li <liphoebe01@gmail.com>
2020-12-05 11:28:42 -0800

    refactoring general -> nsevp and subscriber -> ks

---
+++ src/screens/ks/SubscriberDashboard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import DashboardSection from './components/DashboardSection';
+import '../../styles/SubscriberDashboard.css';
+import '../../styles/Community.css';
+
+class KSDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [],
+      activeBill: null,
+      effectiveCostData: [],
+      mode: 0
+    };
+  }
+
+  seeAllBills() {
+    this.setState({
+      mode: 1
+    });
+  }
+
+  render() {
+    return (
+      <div className="subscriber-page ">
+        <div className="subscriber-main">
+          <div className="subscriber-section">
+            <DashboardSection />
+          </div>
+        </div>
+        {/* <div className="subscriber-side" /> */}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = () => ({
+  // user: state.userData.user,
+  // projectGroup: state.userData.projectGroup,
+  // solarProjects: state.userData.solarProjects
+});
+
+export default connect(mapStateToProps)(KSDashboard);
