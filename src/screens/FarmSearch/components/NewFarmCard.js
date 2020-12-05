fba9e55e27ec94151b7e7b6b35b476d3efa827fd
Andi Halim <andihalim@berkeley.edu>
2020-12-05 17:21:18 -0500

    add hover cursor

--- src/screens/FarmSearch/components/NewFarmCard.js
+++ src/screens/FarmSearch/components/NewFarmCard.js
@@ -5,9 +5,13 @@ import fileIcon from '../../../assets/file-icon.png';
 class NewFarmCard extends React.PureComponent {
   render() {
     return (
-      <div className="newfarm-card">
+      <div className="new-farm-card">
         New Farm Profile
-        <img src={fileIcon} className="newfarm-fileIcon" alt="file-icon" />
+        <img
+          src={fileIcon}
+          className="new-farm-card__file-icon"
+          alt="file-icon"
+        />
       </div>
     );
   }
