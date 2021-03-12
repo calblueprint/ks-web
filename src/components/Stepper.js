bf0643c34cafcd4a49841a63c38071e084d1c059
Myles Domingo <mylesdomingo@berkeley.edu>
2021-03-11 18:31:56 -0800

    added active steps

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -4,24 +4,34 @@ import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 
-const useStyles = makeStyles(theme => ({
+import DoneIcon from '@material-ui/icons/Done';
+
+const useStyles = makeStyles({
   root: {
     width: '100%'
   },
-  backButton: {
-    marginRight: theme.spacing(1)
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1)
-  },
   stepper: {
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+    margin: 0,
+    padding: 0
   },
   label: {
     whiteSpace: 'pre-wrap'
   }
-}));
+});
+
+const useIconStyles = makeStyles({
+  root: {
+    borderRadius: '50%',
+    backgroundColor: 'var(--ks-dark-blue)',
+    width: 36,
+    height: 36,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white'
+  }
+});
 
 function getSteps() {
   return [
@@ -36,16 +46,23 @@ function getSteps() {
   ];
 }
 
+function StepIcon(props) {
+  const classes = useIconStyles();
+  const { completed } = props;
+
+  return <div className={classes.root}>{completed ? <DoneIcon /> : null}</div>;
+}
+
 export default function HorizontalLabelPositionBelowStepper() {
   const classes = useStyles();
   const steps = getSteps();
 
   return (
     <div className={classes.root}>
-      <Stepper className={classes.stepper} alternativeLabel>
+      <Stepper activeStep={5} className={classes.stepper} alternativeLabel>
         {steps.map(label => (
           <Step key={label}>
-            <StepLabel>
+            <StepLabel StepIconComponent={StepIcon}>
               <h3 className={classes.label}>{label}</h3>
             </StepLabel>
           </Step>
