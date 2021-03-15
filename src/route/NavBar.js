33926934ffc9326df21e48151efadcd406ff47c8
Andi Halim <andihalim@berkeley.edu>
2021-03-14 21:02:23 -0700

    ks+nsevp conditionals created

--- src/route/NavBar.js
+++ src/route/NavBar.js
@@ -3,63 +3,50 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import {
-  isAdmin,
+  isNSEVPUser,
+  isKSUser,
   isSignedIn,
-  isOnboarding,
   getCredentials
 } from '@lib/credentials';
-import Logo from '@assets/NSEVP-LOGO.svg';
 import '@styles/NavBar.css';
+import NSEVPLogo from '@assets/NSEVP-LOGO.svg';
+import KSLogo from '@assets/KS-LOGO.svg';
+import DefaultUserIcon from '@assets/defaultUserIcon-small.svg';
 import SettingsDropdown from './SettingsDropdown';
-import DefaultUserIcon from '../assets/defaultUserIcon-small.svg';
 
 class NavBar extends React.PureComponent {
   render() {
     const { user, pathname, history } = this.props;
     const credentials = getCredentials(user);
-
-    if (isOnboarding(credentials)) {
-      return (
-        <div className="nav-bar">
-          <a href="/">
-            <img
-              className="nav-bar__logo"
-              src={Logo}
-              alt="North Shore Economic Vitality Partnership Logo"
-            />
-          </a>
-          <div className="nav-bar__title">
-            <h3>North Shore</h3>
-            <h3>EVP</h3>
-          </div>
-          <nav>
-            {isSignedIn(credentials) && (
-              <ul>
-                <div className="dropdown-safety-box" />
-                <li className="nav-item dropdown-container">
-                  <SettingsDropdown history={history} />
-                </li>
-              </ul>
-            )}
-          </nav>
+    let affiliation;
+    if (isNSEVPUser(credentials)) {
+      affiliation = (
+        <div className="nav-bar__title">
+          <h3>North Shore</h3>
+          <h3>EVP</h3>
         </div>
       );
     }
-    // else, if is signed in and DONE with onboarding
+    if (isKSUser(credentials)) {
+      affiliation = (
+        <img
+          className="nav-bar__kslogo"
+          src={KSLogo}
+          alt="Kamehameha Schools Logo"
+        />
+      );
+    }
 
     return (
       <div className="nav-bar">
         <a href="/">
           <img
-            className="nav-bar__logo"
-            src={Logo}
+            className="nav-bar__nsevplogo"
+            src={NSEVPLogo}
             alt="North Shore Economic Vitality Partnership Logo"
           />
         </a>
-        <div className="nav-bar__title">
-          <h3>North Shore</h3>
-          <h3>EVP</h3>
-        </div>
+        {affiliation}
         <nav>
           {isSignedIn(credentials) && (
             <ul>
@@ -92,15 +79,17 @@ class NavBar extends React.PureComponent {
                     Forecast
                   </NavLink>
                 </li>
-                <li>
-                  <NavLink
-                    to="/referrals"
-                    className="nav-bar__header"
-                    activeClassName="nav-bar__header-active"
-                  >
-                    Referrals
-                  </NavLink>
-                </li>
+                {isNSEVPUser(credentials) && (
+                  <li>
+                    <NavLink
+                      to="/referrals"
+                      className="nav-bar__header"
+                      activeClassName="nav-bar__header-active"
+                    >
+                      Referrals
+                    </NavLink>
+                  </li>
+                )}
               </div>
 
               <div className="nav-right-container">
