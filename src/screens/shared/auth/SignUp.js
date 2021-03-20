c62d9e863fdddcce07e9935c890eb76f5466c4eb
Divi Schmidt <divi@berkeley.edu>
2021-03-20 14:36:19 -0700

    added some fields, no checking

--- src/screens/shared/auth/SignUp.js
+++ src/screens/shared/auth/SignUp.js
@@ -1,9 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class SignUp extends Component {
-  render() {
-    return <div>sign up</div>;
-  }
+import { TextField, FormControl, InputLabel, Select } from '@material-ui/core';
+import '@styles/SignUp.css';
+
+function SignUp() {
+  const [state, setState] = React.useState({
+    age: '',
+    name: 'hai'
+  });
+
+  const handleChange = event => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <TextField required label="First Name" variant="outlined" />
+        <TextField required label="Last Name" variant="outlined" />
+      </div>
+      <div>
+        {/* TODO add validation of email */}
+        <TextField required label="Email" variant="outlined" />
+      </div>
+      <div>
+        <FormControl variant="outlined">
+          <InputLabel>Organization</InputLabel>
+          <Select
+            native
+            label="Organization"
+            value={state.organization}
+            onChange={handleChange}
+          >
+            <option aria-label="" value="" />
+            <option value={'KS'}>KS</option>
+            <option value={'NSEVP'}>NSEVP</option>
+          </Select>
+        </FormControl>
+        <TextField
+          required
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
