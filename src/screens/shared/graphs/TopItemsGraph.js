c8683015881ff735b237b18e2766a984b1a842d6
Andi Halim <andihalim@berkeley.edu>
2021-04-29 22:20:49 -0700

    MVP 1 done. Sorted graphs Top Items only for 1 Harvest Log

--- src/screens/shared/graphs/TopItemsGraph.js
+++ src/screens/shared/graphs/TopItemsGraph.js
@@ -1,35 +1,61 @@
 import React from 'react';
-import {
-  getHarvestLog,
-  getTotalHarvest
-} from '@lib/farmUtils';
+import { getTotalHarvest } from '@lib/farmUtils';
 
 import FarmProfileGraph from './FarmProfileGraph';
 
 class TopItemsGraph extends React.PureComponent {
-  async componentDidMount() {
-    const { farm } = this.props;
-    const harvestLogIds = farm.recentHarvestLogIds;
-    const totalHarvestIds = farm.totalHarvestIds;
-
-    const harvestLogs = await Promise.all(harvestLogIds.map(await getHarvestLog))
-    console.log(harvestLogs)
-
-
-    const totalHarvest = await Promise.all(totalHarvestIds.map(await getTotalHarvest))
-    console.log(totalHarvest)
+  constructor(props) {
+    super(props);
+    this.state = {
+      crops: '',
+      quantitiesFloats: ''
+    };
   }
 
-  getData = () => {
-    // Fill in with Airtable Data
+  async componentDidMount() {
+    const { farm } = this.props;
+    const totalHarvest = await Promise.all(
+      farm.totalHarvestIds.map(await getTotalHarvest)
+    );
+
+    console.log(totalHarvest);
+
+    const { crops, quantities } = totalHarvest[0];
+    const quantitiesFloats = quantities.match(/\d+(?:\.\d+)?/g).map(Number);
+    this.setState({ crops, quantitiesFloats });
+  }
+
+  sortData = (crops, quantitiesFloats) => {
+    const cropsList = crops.split(',');
+    let dict = [];
+    for (let i = 0; i < cropsList.length; i += 1) {
+      cropsList[i] = cropsList[i].replace(/^\s+|\s+$/g, '');
+      dict[i] = [cropsList[i], quantitiesFloats[i]];
+    }
+    dict = dict.sort(function(a, b) {
+      return b[1] - a[1];
+    });
+    console.log(dict);
+
+    const cropsSorted = [];
+    const quantitiesSorted = [];
+    for (let j = 0; j < dict.length; j += 1) {
+      // eslint-disable-next-line prefer-destructuring
+      cropsSorted[j] = dict[j][0];
+      // eslint-disable-next-line prefer-destructuring
+      quantitiesSorted[j] = dict[j][1];
+    }
+
     return {
-      labels: ['Eggplant', 'Tomato', 'Cucumber', 'Melons', 'Apple'],
-      values: [10, 7, 5, 8, 11]
+      labels: cropsSorted,
+      values: quantitiesSorted
     };
   };
 
   render() {
-    const { labels, values } = this.getData();
+    const { crops, quantitiesFloats } = this.state;
+
+    const { labels, values } = this.sortData(crops, quantitiesFloats);
     return <FarmProfileGraph labels={labels} values={values} />;
   }
 }
