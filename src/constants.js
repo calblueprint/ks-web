a76d4c09cba9e0dfbbf53907437dc69d3bc61883
Phoebe Li <liphoebe01@gmail.com>
2021-05-18 16:27:45 -0700

    update scehma, add constant routes, update credentials

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,14 @@ export default {
   // TYPES
   KS_USER: 'KS',
   NSEVP_USER: 'NSEVP',
-  SUPERADMIN: 'Super Admin',
 
   // ROUTES
+  ABOUT_ROUTE: '/',
+  FARM_PROFILE_ROUTE: '/farm/:farmId',
+  FARM_PROFILE_EDIT_ROUTE: '/farm/:farmId/:state',
+  FARM_SEARCH_ROUTE: '/farms',
   HOME_ROUTE: '/',
+  PROFILE_ROUTE: '/profile',
+  REFERRAL_ROUTE: '/referral',
   SIGNUP_ROUTE: '/signup'
 };
--- src/lib/airtable/schema.js
+++ src/lib/airtable/schema.js
@@ -64,7 +64,8 @@ export const Columns = {
     totalHarvestIds: { name: `Total Harvests`, type: `foreignKey-many` },
     gapCertificationId: { name: `GAP Certification`, type: `foreignKey-one` },
     commentfromComments: { name: `Comment (from Comments)`, type: `lookup` },
-    hcCompanyName: { name: `HC Company Name`, type: `text` }
+    hcCompanyName: { name: `HC Company Name`, type: `text` },
+    field31: { name: `Field 31`, type: `text` }
   },
   'GAP Certification': {
     id: { name: `ID`, type: `formula` },
